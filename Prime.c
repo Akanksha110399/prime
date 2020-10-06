78c713362087bfb4867f6498117d29315a09f315
Akanksha110399 <61195841+Akanksha110399@users.noreply.github.com>
2020-10-06 08:40:52 +0530

    Create Prime.c

---
+++ Prime.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int prime(int num,int i)
+{
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       if (num % i == 0)
+       {
+         return 0;
+       }
+       else
+       {
+         return prime(num, i - 1);
+       }       
+    }
+}
+int main()
+{
+	int num,flag;
+	printf("Enter the number which you want to check\n");
+	scanf("%d",&num);
+	flag=prime(num,num/2);
+	if(flag==1)
+	{
+		printf("%d is Prime number\n",num);
+	}
+	else
+	printf("%d is not Prime number\n",num);
+	return 0;
+}
